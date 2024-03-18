07ff1decdda8081ab1b1c39f688ec1e940993714
smith-steve <Steve.A.Dore@outlook.com>
2024-03-18 15:07:02 -0700

    small commit

--- backend/djangocookieauth/djangocookieauth/settings.py
+++ backend/djangocookieauth/djangocookieauth/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api',
     'api.apps.ApiConfig'
 ]
 
@@ -123,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_COOKIE_SAMESITE = 'Strict'
+SESSION_COOKIE_SAMESITE = 'Strict'
